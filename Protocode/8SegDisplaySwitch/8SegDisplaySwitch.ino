406764f60716d06edf853dba14f66c8c93af590f
XXXXKLAUSXXX <146330604+XXXXKLAUSXXX@users.noreply.github.com>
2023-09-28 11:39:10 +0200

    Switch statement for the 7segdisplay almost done

---
+++ Protocode/8SegDisplaySwitch/8SegDisplaySwitch.ino
@@ -0,0 +1,41 @@
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+int floorint = 0;
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+switch (floorint) {
+
+default:
+break;
+
+case 0: // This is the 1 floor
+
+break;
+
+case 1: // This is the 2 floor
+
+break;
+
+case 2: // This is the 3 floor
+
+break;
+
+case 3: // This is the 4 floor
+
+break;
+
+case 4: // this is the 5 floor
+
+break;	
+
+return 0;
+}
+
+}
